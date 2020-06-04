197df8c0e699af19ac40641e2d89fcb0700f2539
Richard <rwebb2305@gmail.com>
2020-06-04 21:15:51 +0100

    refactor: move mocks to separate file
    keeps the test files cleaner
    more refactoring coming

---
+++ server/mocks/mocks.go
@@ -0,0 +1,80 @@
+package mocks
+
+import (
+	"context"
+
+	"github.com/Jaeger2305/du-meine-gute/storage"
+	"github.com/stretchr/testify/mock"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type MongoClient struct {
+	mock.Mock
+}
+
+type MockDatabase struct {
+	Db mock.Mock
+}
+
+type CollectionHelper struct {
+	mock.Mock
+}
+
+type Cursor struct {
+	mock.Mock
+}
+
+type SingleResultHelper struct {
+	mock.Mock
+}
+
+type GameStore struct {
+	mock.Mock
+}
+
+func (mockClient *MongoClient) Database(dbName string, options ...*options.DatabaseOptions) storage.DatabaseHelper {
+	ret := mockClient.Called(dbName)
+	return ret.Get(0).(storage.DatabaseHelper)
+}
+
+func (mockClient *MongoClient) Disconnect(context context.Context) error {
+	return nil
+}
+
+func (mockDatabase *MockDatabase) Collection(collectionName string) storage.CollectionHelper {
+	ret := mockDatabase.Db.Called(collectionName)
+	return ret.Get(0).(storage.CollectionHelper)
+}
+
+func (mockCollection *CollectionHelper) Find(...interface{}) (storage.Cursor, error) {
+	mockCollection.Called()
+	return new(Cursor), nil
+}
+
+func (mockCollection *CollectionHelper) FindOne(ctx context.Context, filter interface{}) storage.SingleResultHelper {
+	ret := mockCollection.Called(ctx, filter)
+	return ret.Get(0).(storage.SingleResultHelper)
+}
+
+func (mockCollection *CollectionHelper) InsertOne(ctx context.Context, document interface{}) (interface{}, error) {
+	ret := mockCollection.Called(ctx, document)
+	return ret.Get(0), ret.Error(1)
+}
+
+func (mockCursor *Cursor) Next(...interface{}) bool {
+	return true
+}
+func (mockCursor *Cursor) Close(...interface{}) error {
+	return nil
+}
+func (mockCursor *Cursor) Decode(...interface{}) error {
+	return nil
+}
+
+func (mockSingleResultHelper *SingleResultHelper) Decode(emptyGame interface{}) error {
+	ret := mockSingleResultHelper.Called(emptyGame)
+	if ret.Get(0) == nil {
+		return nil
+	}
+	return ret.Get(0).(error)
+}
--- server/repository/game_test.go
+++ server/repository/game_test.go
@@ -5,85 +5,13 @@ import (
 	"errors"
 	"testing"
 
-	"github.com/Jaeger2305/du-meine-gute/storage"
+	"github.com/Jaeger2305/du-meine-gute/mocks"
 	databases "github.com/Jaeger2305/du-meine-gute/storage"
 	models "github.com/Jaeger2305/du-meine-gute/storage/models"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
-type MongoClient struct {
-	mock.Mock
-}
-
-type DatabaseHelper struct {
-	db mock.Mock
-}
-
-type CollectionHelper struct {
-	mock.Mock
-}
-
-type Cursor struct {
-	mock.Mock
-}
-
-type SingleResultHelper struct {
-	mock.Mock
-}
-
-type GameStore struct {
-	mock.Mock
-}
-
-func (mockClient *MongoClient) Database(dbName string, options ...*options.DatabaseOptions) storage.DatabaseHelper {
-	ret := mockClient.Called(dbName)
-	return ret.Get(0).(storage.DatabaseHelper)
-}
-
-func (mockClient *MongoClient) Disconnect(context context.Context) error {
-	return nil
-}
-
-func (mockDatabase *DatabaseHelper) Collection(collectionName string) storage.CollectionHelper {
-	ret := mockDatabase.db.Called(collectionName)
-	return ret.Get(0).(storage.CollectionHelper)
-}
-
-func (mockCollection *CollectionHelper) Find(...interface{}) (storage.Cursor, error) {
-	mockCollection.Called()
-	return new(Cursor), nil
-}
-
-func (mockCollection *CollectionHelper) FindOne(ctx context.Context, filter interface{}) storage.SingleResultHelper {
-	ret := mockCollection.Called(ctx, filter)
-	return ret.Get(0).(storage.SingleResultHelper)
-}
-
-func (mockCollection *CollectionHelper) InsertOne(ctx context.Context, document interface{}) (interface{}, error) {
-	ret := mockCollection.Called(ctx, document)
-	return ret.Get(0), ret.Error(1)
-}
-
-func (mockCursor *Cursor) Next(...interface{}) bool {
-	return true
-}
-func (mockCursor *Cursor) Close(...interface{}) error {
-	return nil
-}
-func (mockCursor *Cursor) Decode(...interface{}) error {
-	return nil
-}
-
-func (mockSingleResultHelper *SingleResultHelper) Decode(emptyGame interface{}) error {
-	ret := mockSingleResultHelper.Called(emptyGame)
-	if ret.Get(0) == nil {
-		return nil
-	}
-	return ret.Get(0).(error)
-}
 
 func TestFindOne(t *testing.T) {
 
@@ -95,19 +23,19 @@ func TestFindOne(t *testing.T) {
 	var srHelperCorrect databases.SingleResultHelper
 
 	// Set interfaces implementation to mocked structures
-	clientHelper = &MongoClient{}
-	dbHelper = &DatabaseHelper{}
-	collectionHelper = &CollectionHelper{}
-	srHelperErr = &SingleResultHelper{}
-	srHelperCorrect = &SingleResultHelper{}
+	clientHelper = &mocks.MongoClient{}
+	dbHelper = &mocks.MockDatabase{}
+	collectionHelper = &mocks.CollectionHelper{}
+	srHelperErr = &mocks.SingleResultHelper{}
+	srHelperCorrect = &mocks.SingleResultHelper{}
 
 	// Because interfaces does not implement mock.Mock functions we need to use
 	// type assertion to mock implemented methods
-	srHelperErr.(*SingleResultHelper).
+	srHelperErr.(*mocks.SingleResultHelper).
 		On("Decode", mock.AnythingOfType("*storage.Test")).
 		Return(errors.New("mocked-error"))
 
-	srHelperCorrect.(*SingleResultHelper).
+	srHelperCorrect.(*mocks.SingleResultHelper).
 		On("Decode", mock.AnythingOfType("*storage.Test")).
 		Return(nil).
 		Run(func(args mock.Arguments) {
@@ -115,18 +43,18 @@ func TestFindOne(t *testing.T) {
 			arg.Name = "mocked-game"
 		})
 
-	collectionHelper.(*CollectionHelper).
+	collectionHelper.(*mocks.CollectionHelper).
 		On("FindOne", context.Background(), primitive.M{"error": true}).
 		Return(srHelperErr)
 
-	collectionHelper.(*CollectionHelper).
+	collectionHelper.(*mocks.CollectionHelper).
 		On("FindOne", context.Background(), primitive.M{"error": false}).
 		Return(srHelperCorrect)
 
-	dbHelper.(*DatabaseHelper).db.
+	dbHelper.(*mocks.MockDatabase).Db.
 		On("Collection", "games").Return(collectionHelper)
 
-	clientHelper.(*MongoClient).
+	clientHelper.(*mocks.MongoClient).
 		On("Database", "du-meine-gute").Return(dbHelper)
 
 	// Create new database with mocked Database interface
