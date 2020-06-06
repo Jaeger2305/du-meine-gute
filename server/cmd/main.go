4cf30130113594aa9b908d7bfcd9bdb18f6efa2c
Richard <rwebb2305@gmail.com>
2020-06-06 09:54:39 +0100

    refactor: move route setup to its own file

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -4,38 +4,18 @@ import (
 	"context"
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/Jaeger2305/du-meine-gute/storage"
 	"github.com/spf13/viper"
-
-	"github.com/Jaeger2305/du-meine-gute/handlers"
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
 )
 
 func main() {
 	setupConfig()
-	httpPort := viper.GetString("DMG_PORT")
 	client := storage.NewClient(&storage.MongoConnector{}, viper.GetString("DMG_CONNECTION_STRING"))
 	defer client.Disconnect(context.Background())
+	router := setupRoutes(client)
 
-	router := chi.NewRouter()
-
-	// Middleware setup
-	router.Use(middleware.RequestID)
-	router.Use(middleware.Logger)
-	router.Use(middleware.Recoverer)
-	router.Use(middleware.Timeout(60 * time.Second))
-
-	// REST API
-	// router.Get("/", handlers.ServeFiles)
-	router.Get("/games", handlers.GetGames(client))
-	// router.Get("/games", handlers.HandlerFunc{env, handlers.GetGames})
-	// router.Post("/games/join", handlers.JoinGame)
-	// router.Get("/status", handlers.GetStatus)
-	// router.Get("/games/live", handlers.GetLive)
-
+	httpPort := viper.GetString("DMG_PORT")
 	log.Printf("Running on port %s\n", httpPort)
 	log.Fatal(http.ListenAndServe(":"+httpPort, router))
 }
--- server/cmd/setup.go
+++ server/cmd/setup.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"fmt"
+	"time"
 
+	"github.com/Jaeger2305/du-meine-gute/handlers"
+	"github.com/Jaeger2305/du-meine-gute/storage"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/spf13/viper"
 )
 
@@ -16,4 +21,23 @@ func setupConfig() {
 		panic(fmt.Errorf("Fatal error config file: %s", err))
 	}
 	viper.AutomaticEnv()
+}
+
+func setupRoutes(client storage.Client) *chi.Mux {
+	router := chi.NewRouter()
+
+	// Middleware setup
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.Timeout(60 * time.Second))
+
+	// REST API
+	// router.Get("/", handlers.ServeFiles)
+	router.Get("/games", handlers.GetGames(client))
+	router.Get("/game/{gameID}", handlers.GetGame(client))
+	// router.Post("/games/join", handlers.JoinGame)
+	// router.Get("/status", handlers.GetStatus)
+	// router.Get("/games/live", handlers.GetLive)
+	return router
 }
