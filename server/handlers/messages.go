86ee4505c672aa32c6bf205083e00d239ef54f34
Richard <richard.webb@sgef-df.com>
2020-08-20 23:27:01 +0200

    feat: add playCard basic message

--- server/handlers/messages.go
+++ server/handlers/messages.go
@@ -9,6 +9,7 @@ import (
 	gameRepository "github.com/Jaeger2305/du-meine-gute/repository"
 	"github.com/Jaeger2305/du-meine-gute/storage"
 	cards "github.com/Jaeger2305/du-meine-gute/storage/cards"
+	models "github.com/Jaeger2305/du-meine-gute/storage/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -63,7 +64,72 @@ func drawCard(gameStore storage.Collection, gameID primitive.ObjectID, playerUse
 	return "drawn card"
 }
 
-func playCard() string {
+func playCard(gameStore storage.Collection, gameID primitive.ObjectID, playerUsername string, cardName string) string {
+	// Setup
+	shortTimeoutContext, cancelUpdateGame := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancelUpdateGame()
+
+	// Get the game to update
+	activeGame, getGameErr := gameRepository.FindOne(gameStore, shortTimeoutContext, bson.M{"_id": gameID}) // Should have a projection too.
+	if getGameErr != nil {
+		log.Println("Couldn't get game", gameID.Hex(), getGameErr)
+		return "error"
+	}
+
+	// Get card to play
+	var activePlayer *models.Player
+	for _, player := range activeGame.State.Players {
+		if player.Name == playerUsername {
+			activePlayer = &player
+			break
+		}
+	}
+	if activePlayer == nil {
+		log.Println("Couldn't find player in game", playerUsername, gameID.Hex())
+		return "error"
+	}
+
+	var cardToPlay *models.Card
+	for _, card := range activePlayer.CardsInHand {
+		if card.Name == cardName {
+			cardToPlay = &card
+			break
+		}
+	}
+	if cardToPlay == nil {
+		log.Println("Couldn't find card in player's hand", cardName, playerUsername, gameID.Hex())
+		return "error"
+	}
+
+	// Remove from hand and put into play
+	updateResult, updateError := gameRepository.Update(gameStore, shortTimeoutContext, bson.M{"_id": gameID}, bson.M{
+		"$pull": &bson.M{
+			"state.players.$[activePlayer].cardsInHand": &bson.M{
+				"name": cardToPlay.Name,
+			},
+		},
+		"$push": &bson.M{
+			"state.players.$[activePlayer].cardsInPlay": bson.M{
+				"name": &cardToPlay.Name,
+				"type": &cardToPlay.Type,
+			},
+		},
+	}, options.Update().SetArrayFilters(options.ArrayFilters{
+		Filters: []interface{}{bson.M{"activePlayer.name": playerUsername}},
+	}))
+
+	switch updateError {
+	case nil:
+		log.Println("Updated game successfully", gameID, "with count", updateResult.ModifiedCount)
+	case mongo.ErrNoDocuments:
+		log.Println("no documents found", gameID)
+		return "error"
+	default:
+		log.Println("unknown error", gameID)
+		return "error"
+	}
+
+	// Return success
 	return "played card"
 }
 
@@ -94,7 +160,7 @@ func RouteIncomingMessage(message string, gameStore storage.Collection, idToFetc
 		drawCard(gameStore, idToFetch, playerUsername)
 		return "success", nil
 	case "playCard":
-		playCard()
+		playCard(gameStore, idToFetch, playerUsername, "test1")
 		return "success", nil
 	case "ready":
 		ready(gameStore, idToFetch)
