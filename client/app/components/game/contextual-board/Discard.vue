7e99158a0cc97e8ae4b10d31acc96faa95e1fc59
Jaeger2305 <rwebb2305@gmail.com>
2020-11-15 16:55:22 +0100

    feat: add the end step to the discard banner

--- client/app/components/game/contextual-board/Discard.vue
+++ client/app/components/game/contextual-board/Discard.vue
@@ -1,10 +1,10 @@
 <template>
   <Page actionBarHidden="true" class="cover-image">
-    <GridLayout columns="*,2*,*" rows="*">
-      <Notification header="Start" />
+    <GridLayout columns="*,2*,*" rows="*" class="grid-container">
+      <Notification class="grid-item" header="Start" />
       <NotificationMessageContainer
+        class="grid-item"
         column="1"
-        style="margin: 15px;"
         :messages="[
           {
             content: 'Don\'t have the right card?',
@@ -14,7 +14,15 @@
           },
         ]"
       />
-      <Button column="2" @tap="endStep">END</Button>
+      <FlexboxLayout
+        column="2"
+        class="grid-item"
+        alignItems="stretch"
+        justifyContent="center"
+        flexDirection="column"
+      >
+        <Button @tap="endStep" class="button">END</Button>
+      </FlexboxLayout>
     </GridLayout>
   </Page>
 </template>
@@ -50,5 +58,44 @@ export default {
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
+}
+
+/* needed to make the grid spacing even between items or at the end */
+.grid-container {
+  margin-left: 15px;
+  margin-right: 15px;
+}
+
+.grid-item {
+  margin: 25px 15px 25px 15px;
+}
+
+.button {
+  font-family: "Grandstander", "Grandstander-Regular";
+  font-size: 30px;
+  border-width: 7px;
+  border-color: lightblue;
+  border-radius: 7px;
+  background: rgb(188, 209, 215);
+  background: linear-gradient(
+    0deg,
+    rgba(188, 209, 215, 1) 0%,
+    rgba(211, 229, 235, 1) 25%,
+    rgba(211, 229, 235, 1) 75%,
+    rgba(237, 246, 249, 1) 100%
+  );
+  margin: 5px 0px 5px 0px;
+}
+
+.button:highlighted {
+  border-color: #8ebccc;
+  background: rgb(188, 209, 215);
+  background: linear-gradient(
+    0deg,
+    rgb(209, 225, 230) 0%,
+    rgb(227, 240, 245) 25%,
+    rgb(227, 242, 247) 75%,
+    rgb(250, 254, 255) 100%
+  );
 }
 </style>
