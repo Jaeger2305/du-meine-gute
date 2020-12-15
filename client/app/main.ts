5e2596b98f9ab54cf03826ba82b90d92e7ea81b0
Jaeger2305 <rwebb2305@gmail.com>
2020-12-15 11:24:23 +0000

    chore: remove unused components from app registration
    I prefer explicit imports, but vue nativescript needs the initial frames loaded up front, which is why some must remain

--- client/app/main.ts
+++ client/app/main.ts
@@ -21,12 +21,8 @@ import store from "./store";
 Vue.config.silent = TNS_ENV === "production";
 import GameListItem from "./components/GameListItem.vue";
 import Banner from "./components/game/contextual-board/Banner.vue";
-import Card from "./components/Card.vue";
-import Deck from "./components/game/Deck.vue";
-import AvailableActions from "./components/game/AvailableActions.vue";
 import PlayerHand from "./components/game/PlayerHand.vue";
 import Factories from "./components/game/Factories.vue";
-import Resources from "./components/game/Resources.vue";
 
 Vue.registerElement(
   "ShadowedLabel",
@@ -36,11 +32,7 @@ Vue.registerElement(
 Vue.component("App", App); // Locally registering inside of the Lobby didn't seem to work. Maybe it needs a different frame.
 Vue.component("GameListItem", GameListItem); // Locally registering inside of the Lobby didn't seem to work. Maybe it needs a different frame.
 Vue.component("Banner", Banner);
-Vue.component("Card", Card);
-Vue.component("Deck", Deck);
-Vue.component("Resources", Resources);
 Vue.component("Factories", Factories);
-Vue.component("AvailableActions", AvailableActions);
 Vue.component("PlayerHand", PlayerHand);
 
 new Vue({
